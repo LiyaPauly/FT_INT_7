267f54a8f4b0c3af605011249e70902a3e7ff8a9
Liya <liya.pauly96@gmail.com>
2025-01-09 02:09:09 +0100

    task2 added

---
+++ JS3/main2.js
@@ -0,0 +1,8 @@
+//Add the string “1” and the digit 2 (both operands must be in variables), and get a mathematically correct answer.
+
+let str = "1";
+let num = 2;
+// Convert the string to a number and add
+let Output = Number(str) + num;
+
+console.log(`The output is :`, Output); // Output: 3
